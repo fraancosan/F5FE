290a0564d35c1cb61816e3bf5bfca34a04e5fb32
Franco Sanchez <francosanchez8102@gmail.com>
2025-08-21 18:04:12 -0300

    update: add signIn function

--- src/app/services/db/users.ts
+++ src/app/services/db/users.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { LocalStorage } from '../common/local-storage';
 import { Navigation } from '../common/navigation';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +22,18 @@ export class Users {
     if (sendLogin) {
       this.navSv.toPageTop('/sign-in');
     }
+  }
+
+  signIn(data: { mail: string; contraseña: string }) {
+    this.localStorageSv.removeItem('token');
+    return this.http.post(this.urlBack + 'usuarios/login', data).pipe(
+      tap((res: any) => {
+        this.localStorageSv.setItem('token', res.token);
+      }),
+      catchError((error: HttpErrorResponse) => {
+        // habria que incluir un servicio de notificaciones
+        return throwError(() => error);
+      })
+    );
   }
 }
