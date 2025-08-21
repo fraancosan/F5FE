e068dcbd7c49209850ff9b9c995401eed39a953d
Franco Sanchez <francosanchez8102@gmail.com>
2025-08-21 18:04:24 -0300

    feat: add routing and components for home, sign-in, and sign-up pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/home/home'),
+  },
+
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./pages/sign-in/sign-in'),
+  },
+
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./pages/sign-up/sign-up'),
+  },
+
+  { path: '**', redirectTo: 'inicio' },
+];
