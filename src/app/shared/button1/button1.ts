4ae514587904b186c16bddfcb0d64fd28239e703
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-01 13:56:51 -0300

    style: create button1 component

---
+++ src/app/shared/button1/button1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button1',
+  imports: [],
+  templateUrl: './button1.html',
+  styleUrl: './button1.css'
+})
+export class Button1 {
+
+}
