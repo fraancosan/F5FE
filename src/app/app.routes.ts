541cf04c46a1f8d52e760bcf5cb4558790849a50
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-03 16:38:35 -0300

    feat: update routing configuration to streamline landing and main layout paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,43 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // Landing directo
   {
-    path: 'landing',
-    loadComponent: () => import('./pages/landing/landing'),
+    path: '',
+    loadComponent: () =>
+      import('./pages/landing/landing').then((m) => m.default),
   },
 
+  // Grupo con layout principal (header + footer)
   {
-    path: 'inicio',
-    loadComponent: () => import('./pages/home/home'),
+    path: '',
+    loadComponent: () =>
+      import('./pages/home-layout/home-layout').then((m) => m.HomeLayout),
+    children: [
+      {
+        path: 'inicio',
+        loadComponent: () => import('./pages/home/home').then((m) => m.default),
+      },
+      {
+        path: 'iniciar-sesion',
+        loadComponent: () =>
+          import('./pages/sign-in/sign-in').then((m) => m.default),
+      },
+      {
+        path: 'registrarse',
+        loadComponent: () =>
+          import('./pages/sign-up/sign-up').then((m) => m.default),
+      },
+      {
+        path: 'reserva',
+        loadComponent: () =>
+          import('./pages/reserva/reserva').then((m) => m.default),
+      },
+    ],
   },
 
-  {
-    path: 'iniciar-sesion',
-    loadComponent: () => import('./pages/sign-in/sign-in'),
-  },
+  { path: 'sign-in', redirectTo: 'iniciar-sesion', pathMatch: 'full' },
+  { path: 'sign-up', redirectTo: 'registrarse', pathMatch: 'full' },
 
-  {
-    path: 'registrarse',
-    loadComponent: () => import('./pages/sign-up/sign-up'),
-  },
-
-  {
-    path: 'reserva',
-    loadComponent: () => import('./pages/reserva/reserva'),
-  },
-
-  {
-    path: 'sign-in',
-    redirectTo: 'iniciar-sesion',
-  },
-
-  {
-    path: 'sign-up',
-    redirectTo: 'registrarse',
-  },
-
-  { path: '**', redirectTo: 'landing' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
