1bb53f58481d374abba492b9d70240e6731ca164
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-02 22:19:52 -0300

    style: update button1 component to accept dynamic text input

--- src/app/shared/button1/button1.ts
+++ src/app/shared/button1/button1.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button1',
   imports: [],
   templateUrl: './button1.html',
-  styleUrl: './button1.css'
+  styleUrl: './button1.css',
 })
 export class Button1 {
-
+  @Input() text: string = '';
 }
