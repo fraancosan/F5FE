19d829f032ba6ae092b7fb3521a7b96c020fbc39
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-03 16:37:49 -0300

    feat: add HomeLayout component to encapsulate menu interface

---
+++ src/app/pages/home-layout/home-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [RouterOutlet],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.css',
+})
+export class HomeLayout {}
