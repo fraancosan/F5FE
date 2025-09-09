c4af715ab5243596a62fed3a2f79e0a9ab076c70
Gino Fina <gino_fina@hotmail.com>
2025-09-08 23:31:01 -0300

    style: create card component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Card } from '../../shared/card/card';
+import { faTrophy, faCalendar, faMagnifyingGlass, faPeopleGroup, faGear, faMessage } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Card],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export default class Home {}
+export default class Home {
+  faTrophy = faTrophy;
+  faCalendar = faCalendar;
+  faMagnifyingGlass = faMagnifyingGlass;
+  faPeopleGroup = faPeopleGroup;
+  faGear = faGear;
+  faMessage = faMessage;
+}
