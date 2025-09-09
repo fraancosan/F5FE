1f4bb6bb94195bb700a1403420ead8428b08cf64
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-08 22:52:54 -0300

    feat: update home page layout with card menus and adjust styles

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CardMenu } from '../../shared/card-menu/card-menu';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+import {
+  faMagnifyingGlass,
+  faTrophy,
+  faPeopleGroup,
+  faGear,
+  faMessage,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardMenu],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export default class Home {}
+export default class Home {
+  faCalendar = faCalendar;
+  faMagnifyingGlass = faMagnifyingGlass;
+  faTrophy = faTrophy;
+  faPeopleGroup = faPeopleGroup;
+  faGear = faGear;
+  faMessage = faMessage;
+}
