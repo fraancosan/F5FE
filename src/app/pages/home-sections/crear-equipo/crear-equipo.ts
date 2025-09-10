37ed5be6af0c2d911a751681ed5ee7c492bb0de4
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-09 23:12:41 -0300

    feat: create crear-equipo controller in home-section

---
+++ src/app/pages/home-sections/crear-equipo/crear-equipo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-equipo',
+  imports: [],
+  templateUrl: './crear-equipo.html',
+  styleUrl: './crear-equipo.css',
+})
+export default class CrearEquipo {}
