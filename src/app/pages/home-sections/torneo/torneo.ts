8eed665368fe7f8cf1c7362d2fbd5639d3c20024
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-09-09 23:12:08 -0300

    feat: create turno controller in home-section

---
+++ src/app/pages/home-sections/torneo/torneo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-torneo',
+  imports: [],
+  templateUrl: './torneo.html',
+  styleUrl: './torneo.css',
+})
+export default class Torneo {}
