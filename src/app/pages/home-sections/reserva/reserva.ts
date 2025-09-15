e27173c6a499ca87a8758e795184ace1ab47cd67
Gino Fina <gino_fina@hotmail.com>
2025-09-15 18:30:36 -0300

    create: reserva component

--- src/app/pages/home-sections/reserva/reserva.ts
+++ src/app/pages/home-sections/reserva/reserva.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Button3 } from '../../../shared/button3/button3';
+import { Button4 } from '../../../shared/button4/button4';
+import { faArrowLeft, faCalendar, faClock, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reserva',
-  imports: [],
+  imports: [Button3, Button4, FontAwesomeModule],
   templateUrl: './reserva.html',
   styleUrl: './reserva.css',
 })
-export default class Reserva {}
+export default class Reserva {
+  faArrowLeft = faArrowLeft;
+  faCalendar = faCalendar;
+  faClock = faClock;
+  faChevronDown = faChevronDown;
+
+  constructor(private router: Router) {}
+
+  goToMenu() {
+    this.router.navigate(['/inicio']);
+  }
+}
