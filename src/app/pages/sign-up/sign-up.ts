c46aa881de34b972933fcc52fbea9cc5efad70ce
Franco Sanchez <francosanchez8102@gmail.com>
2025-10-13 18:30:11 -0300

    update: now using input components

--- src/app/pages/sign-up/sign-up.ts
+++ src/app/pages/sign-up/sign-up.ts
@@ -4,18 +4,79 @@ import { Button1 } from '../../shared/button1/button1';
 import { faArrowLeft, faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputString } from '../../shared/input-string/input-string';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [Button1, Button2, FontAwesomeModule],
+  imports: [
+    Button1,
+    Button2,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    InputString,
+  ],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
 export default class SignUp {
   faArrowLeft = faArrowLeft;
   faCalendar = faCalendar;
+  form: FormGroup;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.form = fb.group({
+      mail: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-zA-Z]{2,}$'),
+          Validators.maxLength(60),
+        ],
+      ],
+      contraseña: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(60),
+        ],
+      ],
+      dni: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]{7,8}$'),
+          Validators.minLength(7),
+          Validators.maxLength(8),
+        ],
+      ],
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-ZÀ-ÿ\\s]{2,60}$'),
+          Validators.minLength(2),
+          Validators.maxLength(60),
+        ],
+      ],
+      telefono: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]{7,30}$'),
+          Validators.minLength(7),
+          Validators.maxLength(30),
+        ],
+      ],
+    });
+  }
 
   goToMenu() {
     this.router.navigate(['/inicio']);
