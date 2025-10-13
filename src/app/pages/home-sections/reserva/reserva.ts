dddcd01eb1f56b6e5e9d071ae058b3cbfb6cdf64
Franco Sanchez <francosanchez8102@gmail.com>
2025-10-13 20:07:51 -0300

    update: now using input date component

--- src/app/pages/home-sections/reserva/reserva.ts
+++ src/app/pages/home-sections/reserva/reserva.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { Button3 } from '../../../shared/button3/button3';
 import { Button4 } from '../../../shared/button4/button4';
-import { faArrowLeft, faCalendar, faClock, faChevronDown, faUtensils, faUserPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faCalendar,
+  faClock,
+  faChevronDown,
+  faUtensils,
+  faUserPlus,
+  faCheck,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { InputDate } from '../../../shared/input-date/input-date';
 
 @Component({
   selector: 'app-reserva',
-  imports: [Button3, Button4, FontAwesomeModule, FormsModule],
+  imports: [Button3, Button4, FontAwesomeModule, FormsModule, InputDate],
   templateUrl: './reserva.html',
   styleUrl: './reserva.css',
 })
@@ -21,6 +30,9 @@ export default class Reserva {
   faUserPlus = faUserPlus;
   faCheck = faCheck;
 
+  minDate: Date = new Date();
+  maxDate: Date = new Date(new Date().setMonth(this.minDate.getMonth() + 1)); // tryhardcodeado a un mes, buscar por backend
+
   base = 15000;
   parrilla = false;
   rival = false;
@@ -29,6 +41,10 @@ export default class Reserva {
 
   goToMenu() {
     this.router.navigate(['/inicio']);
+  }
+
+  onDateSelected(date: string) {
+    console.log('Fecha seleccionada:', date);
   }
 
   get total(): number {
