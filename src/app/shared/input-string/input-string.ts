724e68a8903859feb46d002ec02d130d9ec08ee6
Franco Sanchez <francosanchez8102@gmail.com>
2025-10-13 17:22:53 -0300

    feat: create input string component

---
+++ src/app/shared/input-string/input-string.ts
@@ -0,0 +1,64 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { ValidatorsService } from '../../services/common/validators';
+import {
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-input-string',
+  imports: [FaIconComponent, ReactiveFormsModule],
+  templateUrl: './input-string.html',
+  styleUrl: './input-string.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputString),
+      multi: true,
+    },
+  ],
+})
+export class InputString {
+  @Input() icon?: IconProp;
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: 'text' | 'password' | 'email' = 'text';
+  @Input() form?: FormGroup;
+  @Input() name: string = '';
+  @Input() required: boolean = false;
+  @Input() prefixMessage?: string;
+
+  constructor(public vs: ValidatorsService) {}
+
+  value: string = '';
+  disabled = false;
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  handleInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+  }
+}
