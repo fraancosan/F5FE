d7c4a68b2039646b17a6ed4e13a64ffb422c4896
Franco Sanchez <francosanchez8102@gmail.com>
2025-11-27 19:30:49 -0300

    feat: update go-back component

--- src/app/shared/go-back/go-back.ts
+++ src/app/shared/go-back/go-back.ts
@@ -2,16 +2,18 @@ import { Component, HostListener, Input } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Navigation } from '../../services/common/navigation';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-go-back',
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, NgClass],
   templateUrl: './go-back.html',
   styleUrl: './go-back.css',
 })
 export class GoBack {
   @Input() url: string = '';
   @Input() label: string = '';
+  @Input() mode: 'light' | 'dark' = 'light';
 
   faArrowLeft = faArrowLeft;
 
