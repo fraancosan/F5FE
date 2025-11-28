9c85fca2cb02cdd77e7ab37fb6d6d921bcf09c1f
Franco Sanchez <francosanchez8102@gmail.com>
2025-11-28 20:50:40 -0300

    feat: implement guards in routes

--- src/app/guards/admin-guard.ts
+++ src/app/guards/admin-guard.ts
@@ -2,14 +2,19 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { Auth } from '../services/common/auth';
 import { Navigation } from '../services/common/navigation';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const auth = inject(Auth);
   const nav = inject(Navigation);
+  const notify = inject(MatSnackBar);
 
   if (auth.admin()) {
     return true;
   } else {
+    notify.open('Primero debe iniciar sesión como Administrador', 'Cerrar', {
+      duration: 5000,
+    });
     nav.toPageTop('/iniciar-sesion');
     return false;
   }
--- src/app/guards/user-guard.ts
+++ src/app/guards/user-guard.ts
@@ -2,14 +2,19 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { Auth } from '../services/common/auth';
 import { Navigation } from '../services/common/navigation';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const userGuard: CanActivateFn = (route, state) => {
   const auth = inject(Auth);
   const nav = inject(Navigation);
+  const notify = inject(MatSnackBar);
 
   if (auth.user()) {
     return true;
   } else {
+    notify.open('Primero debe iniciar sesión', 'Cerrar', {
+      duration: 5000,
+    });
     nav.toPageTop('/iniciar-sesion');
     return false;
   }
