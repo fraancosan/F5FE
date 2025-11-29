507fc84815af779b004f9f865a577c1fb6776d00
Franco Sanchez <francosanchez8102@gmail.com>
2025-11-28 22:10:48 -0300

    feat: implement fecha and hora selection of turnos

--- src/app/pages/home-sections/reserva/reserva.ts
+++ src/app/pages/home-sections/reserva/reserva.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { InputDate } from '../../../shared/input-date/input-date';
 import { GoBack } from '../../../shared/go-back/go-back';
 import { InputCheckBox } from '../../../shared/input-check-box/input-check-box';
+import { Turnos } from '../../../services/db/turnos';
 
 @Component({
   selector: 'app-reserva',
@@ -38,14 +39,54 @@ export default class Reserva {
   faUserPlus = faUserPlus;
 
   minDate: Date = new Date();
-  maxDate: Date = new Date(new Date().setMonth(this.minDate.getMonth() + 1)); // tryhardcodeado a un mes, buscar por backend
+  maxDate: Date = new Date();
+
+  turnosDisponibles: {
+    fecha: string;
+    horarios: { disponible: boolean; hora: string }[];
+  }[] = [];
+  horasDisponibles: { disponible: boolean; hora: string }[] = [
+    { disponible: false, hora: 'Seleccione un turno' },
+  ];
+
+  hora = 'Seleccione un turno';
+  fecha = '';
 
   base = 15000;
   parrilla = false;
   rival = false;
 
+  constructor(private turnosService: Turnos) {}
+
   onDateSelected(date: string) {
-    console.log('Fecha seleccionada:', date);
+    this.fecha = date;
+    const turno = this.turnosDisponibles.find((t) => t.fecha === date);
+    if (turno) {
+      let disponible = false;
+      for (const horario of turno.horarios) {
+        if (horario.disponible) {
+          disponible = true;
+          break;
+        }
+      }
+      this.horasDisponibles = disponible
+        ? turno.horarios
+        : [{ disponible: false, hora: 'No hay turnos disponibles' }];
+    } else {
+      this.horasDisponibles = [
+        { disponible: false, hora: 'No hay turnos disponibles' },
+      ];
+    }
+    this.hora = this.horasDisponibles[0].hora;
+  }
+
+  ngOnInit() {
+    this.turnosService.getDisponibles().subscribe((data) => {
+      console.log(data);
+      this.turnosDisponibles = data;
+      this.minDate = data[0].fecha;
+      this.maxDate = data[data.length - 1].fecha;
+    });
   }
 
   get total(): number {
