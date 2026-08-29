e9e9e5196edd170e64356fb32c63307760fb10ef
Franco Sanchez <francosanchez8102@gmail.com>
2026-08-29 16:50:22 -0300

    fix: style of components

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://localhost:3000/',
+  // urlBackend: 'http://localhost:3000/',
+  urlBackend: 'https://rodo-app.francosanchez.com.ar/',
 };
